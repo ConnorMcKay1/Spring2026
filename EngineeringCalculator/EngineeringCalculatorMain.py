b031d801e44eb21c2a882016ff73a24a16dbff7d
Connor <mckayc1@go.stockton.edu>
2026-02-21 23:36:29 -0500

    Starting Web App Project

---
+++ EngineeringCalculator/EngineeringCalculatorMain.py
@@ -0,0 +1,34 @@
+print("hello?!")
+
+
+
+'''
+
+
+    --> steel, concrete, wood, masonry, and aluminum
+        Young's Modulus / Poissons Ratio
+Structural
+-moment of intertia
+-bending stress
+-shear stress
+-deflection
+
+
+    --> https://mccord.cm.utexas.edu/courses/fall2014/ch301/thermoequations.php
+        https://atmos.uw.edu/~robwood/teaching/535/ThermoEquations_2014.pdf
+        https://web.mit.edu/16.unified/www/FALL/thermodynamics/EquationCompendium.pdf
+Thermal
+-Fourier's Law of Conduction
+-Newton's Law of Cooling
+
+
+    --> https://cfdland.com/the-essential-fluid-dynamics-equations/
+Fluid Dynamics
+-The Continuity Equation
+-Navier-Stokes Equations
+-The Energy Equation
+-Fluid Dynamics Bernoulli Equation
+-Euler's Equations
+
+
+'''
