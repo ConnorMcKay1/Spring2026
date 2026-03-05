eb19826a8986968eda8fbaf7636b8bdca212ed3c
Connor <mckayc1@go.stockton.edu>
2026-03-04 22:12:50 -0500

    Starting AppEng HW1

--- AppliedProbability&Statistics/HW1-AppliedProb.py
+++ AppliedProbability&Statistics/HW1-AppliedProb.py
@@ -27,15 +27,15 @@ with breast cancer in a city hospital:
 57, 38, 48, 69, 86, 81, 72, 65, 53, 41}
 
 
-$S--> Write a small program to compute the average value of the dataset and the median.
---> Construct a frequency distribution of the dataset using Sturges's rule for the number of
+1--> Write a small program to compute the average value of the dataset and the median.
+2--> Construct a frequency distribution of the dataset using Sturges's rule for the number of
 classes and rounding up the obtained expression.
---> Draw histogram of the frequency distribution and the frequency polygon.
---> Draw histogram of the cumulative frequency distribution and the ogive (less than type).
---> Illustrate how you would find the median of the frequency distribution you constructed.
+3--> Draw histogram of the frequency distribution and the frequency polygon.
+4--> Draw histogram of the cumulative frequency distribution and the ogive (less than type).
+5--> Illustrate how you would find the median of the frequency distribution you constructed.
 How does it compare with the true median that you get from the above dataset (provide
 the absolute value of the difference of the two)?
---> Compute the arithmetic average of the frequency distribution using class marks and 
+6--> Compute the arithmetic average of the frequency distribution using class marks and 
 compare the value with the true average of the dataset you computed with your program.
 
 '''
@@ -50,7 +50,7 @@ UnSortedDataSet = [36, 59, 78, 35, 53, 44, 19, 88, 39, 47,
 
 # list of ages in ascending order
 dataSet = sorted(UnSortedDataSet)
-print(dataSet)
+#print(dataSet)
 
 
 
@@ -61,16 +61,16 @@ def ArithmeticMean(dataSet):
     print(total)
     return total/len(dataSet)
 
-'''
+
 arithmeticMean = ArithmeticMean(dataSet)
-print(arithmeticMean)
-'''
+print("Arithmetic Mean of Values: ",arithmeticMean)
+
 
 
 # finds the Median of the dataSet
 def Median(dataSet):
     N = len(dataSet)
-    print(N)
+    #print(N)
 
     if N % 2 != 0:
         # if N is odd
@@ -85,19 +85,19 @@ def Median(dataSet):
 
         median = ((upper-1) + (lower-1))/2
         
-        print(f"Number of values: {N} , Upper/Lower Index {math.ceil(medianIndex)} , {math.floor(medianIndex)} , Upper/Lower Element: {upper} , {lower} , Median: {median}")
+        print(f"Number of values: {N} , Upper/Lower Index {math.ceil(medianIndex)} , {math.floor(medianIndex)} , Upper/Lower Element: {upper} , {lower} , Median: {median} \n")
 
 
-'''
+
 Median(dataSet)
-'''
+
 
 
 def FrequencyDistribution(dataSet):
     N = len(dataSet)
 
     # Number of classes
-    K = math.ceil(1 + 3.322*(math.log(N)))
+    K = math.ceil(1 + 3.322*(math.log10(N)))
 
     # Range
     R = max(dataSet) - min(dataSet)
@@ -109,7 +109,7 @@ def FrequencyDistribution(dataSet):
 
 N, K, classwidth = FrequencyDistribution(dataSet)
 
-print(f'length of N: {N}, number of classes {K}, class size {classwidth}')
+print(f'length of N: {N}, number of classes {K}, class size {classwidth} \n')
 
 
 
@@ -131,7 +131,7 @@ def ClassMidpoints(values, classwidth, K):
     return midPoints
 
 midPoints = ClassMidpoints(dataSet, classwidth, K)
-print(midPoints)
+#print(midPoints)
 
 
 
@@ -140,7 +140,7 @@ def ClassBoundaries(values, classwidth, K):
     lowerBoundary = min(values) - 0.5
     return [lowerBoundary + i * classwidth for i in range(K + 1)]
 
-print(ClassBoundaries(dataSet, classwidth, K), "\n")
+#print(ClassBoundaries(dataSet, classwidth, K), "\n")
 
 
 
@@ -163,7 +163,7 @@ table.auto_set_font_size(False)
 table.set_fontsize(10)
 table.scale(1.2, 1.2)
 
-#plt.show()         # Just un-comment to display the table
+plt.show()         # Just un-comment to display the table
 
 
 
