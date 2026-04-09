4d359f0691d345205a3cf7d8b1cc4271aa7887d1
Connor <mckayc1@go.stockton.edu>
2026-04-08 23:33:29 -0400

    reading data to terminal

--- AppliedProbability&Statistics/AppliedProb&StatsConcrete/main.py
+++ AppliedProbability&Statistics/AppliedProb&StatsConcrete/main.py
@@ -1,3 +1,30 @@
-print("test test turnip")
+'''
+SIMPLE RELATIONSHIPS:
+    Which variables are related to the output?
+    Are relationships linear or monotonic?
+        
+PREDICTIVE MODELING:
+    How well can I predict the output from the inputs?
+    ****Which variables matter most?****  use this to find out which variable affects strenght the most?
+
+EFFECTS OF INTERACTION:
+    Do variables combine in not obvious ways?
 
 
+'''
+
+
+
+print("test test turnip \n")
+
+
+dataFile = "C:/Users/cmcka/OneDrive/Desktop/Spring2026/AppliedProbability&Statistics/AppliedProb&StatsConcrete/concrete_compressive_strength.csv"
+
+def DataReadIn(csvFile):
+    with open(csvFile, 'r') as file:
+        for line in file:
+            print(line.strip())
+
+
+
+DataReadIn(dataFile)
